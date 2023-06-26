1a93c03c765942078e2fc1f4274c09d38ad226e0
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2023-06-26 18:55:57 +0300

    Added tests

---
+++ lib/src/test/java/com/streamlined/practice/HeapSorterTest.java
@@ -0,0 +1,49 @@
+package com.streamlined.practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HeapSorterTest {
+
+	@Test
+	@DisplayName("test sorting in ascending order")
+	void testAscendingOrder() {
+
+		final int start = 1;
+		final int finish = 10_000;
+		var sampleData = IntStream.range(start, finish).mapToObj(Integer::valueOf).toList();
+
+		var toBeSorted = new ArrayList<>(sampleData);
+		Collections.shuffle(toBeSorted);
+
+		var sortedData = new HeapSorter<Integer>(Comparator.naturalOrder(), toBeSorted).getSorted();
+
+		assertEquals(sampleData, sortedData);
+
+	}
+
+	@Test
+	@DisplayName("test sorting in descending order")
+	void testDescendingOrder() {
+
+		final int start = 1;
+		final int finish = 10_000;
+		var sampleData = IntStream.range(start, finish).map(x -> finish - x).mapToObj(Integer::valueOf).toList();
+
+		var toBeSorted = new ArrayList<>(sampleData);
+		Collections.shuffle(toBeSorted);
+
+		var sortedData = new HeapSorter<Integer>(Comparator.reverseOrder(), toBeSorted).getSorted();
+
+		assertEquals(sampleData, sortedData);
+
+	}
+
+}
